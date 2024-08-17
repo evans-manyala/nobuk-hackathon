1713f1518b60354d00794afb40fb9307d819bcae
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2024-08-17 19:37:49 +0300

    fixes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env node
 const express = require('express');
 const cors = require("cors");
@@ -12,46 +11,27 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(express.json());
-=======
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-
-const app = express()
-const PORT = process.env.PORT || 5000
-
->>>>>>> 5bf6c06 (payments)
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"));
-<<<<<<< HEAD
 // Routes
-=======
->>>>>>> 5bf6c06 (payments)
 
 const { connect_database } = require("./db/connect");
 connect_database()
 
 //
-<<<<<<< HEAD
 const authRoutes = require('./routes/auth');
 const businessRoutes = require("./routes/business");
 const payerDetailRoutes = require("./routes/payerdetails");
 const paymentLinksRoutes = require("./routes/paylinks");
 const paymentsRoutes = require("./routes/payments");
-=======
-const businessRoutes = require("./routes/business");
-const payerDetailRoutes = require("./routes/payerdetails");
-const paymentLinksRoutes = require("./routes/paylinks");
->>>>>>> 5bf6c06 (payments)
 
 // Base Route
 app.get("/", (req, res) => {
     res.send({ msg: "Server is running!!" }).status(200)
 });
 
-<<<<<<< HEAD
 app.use('/auth', authRoutes);
 app.use("/api/v0/business", businessRoutes)
 app.use("/api/v0/payerdetails", payerDetailRoutes);
@@ -61,12 +41,3 @@ app.use("/api/v0/payments", paymentsRoutes);
 app.listen(PORT, () => {
     console.log(`The server is running on: http://localhost:${PORT}`)
 })
-=======
-app.use("/api/v0/business", businessRoutes)
-app.use("/api/v0/payerdetails", payerDetailRoutes);
-app.use("/api/v0/payment-links", paymentLinksRoutes);
-
-app.listen(PORT, () => {
-    console.log(`The server is running on: http://localhost:${PORT}`)
-})
->>>>>>> 5bf6c06 (payments)
